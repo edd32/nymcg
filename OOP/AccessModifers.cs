366310901ec872793acff43ee1d2b39ffa59fc94
Ben Spencer <ben.richard.spencer@me.com>
2017-01-31 22:07:30 -0500

    accessibility

---
+++ OOP/AccessModifers.cs
@@ -0,0 +1,32 @@
+public class AccessibilityClass
+{
+    private string _myField;
+
+    public string MyProperty
+    {
+        get { return _myField;  }
+        set{ _myField = value;  }
+    }
+}
+
+public class BaseClass
+{
+    private int _privateField = 42;
+    protected int _protectedField = 42;
+
+    private void MyPrivateMethod() { }
+    protected void MyProtectedMethod() { }
+}
+
+public class DerivedClass : BaseClass
+{
+    public void MyDerivedMethod()
+    {
+        // _privateField = 41; // Not OK, this will generate a compile error
+        _protectedField = 43; // OK, protected fields can be accessed
+
+
+        // MyPrivateMethod(); // Not OK, this will generate a compile error
+        MyProtectedMethod(); // OK, protected methods can be accessed
+    }
+}
