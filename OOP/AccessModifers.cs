3f2348b669f18e9402b76b87f0de079522a24a81
Ben Spencer <ben.richard.spencer@me.com>
2017-01-31 22:59:21 -0500

    methods

--- OOP/AccessModifers.cs
+++ OOP/AccessModifers.cs
@@ -14,18 +14,18 @@ public class BaseClass
     private int _privateField = 40;
     protected int _protectedField = 40;
 
-    private void MyPrivateMethod() { }
-    protected void MyProtectedMethod() { }
+    private void PrivateMethod() { }
+    protected void ProtectedMethod() { }
 }
 
 public class DerivedClass : BaseClass
 {
-    public void MyDerivedMethod()
+    public void DerivedMethod()
     {
-        // _privateField = 41; // Not OK, this will generate a compile error
+        // _privateField = 41; // Not OK, compile error
         _protectedField = 43; // OK, protected fields can be accessed
 
-        // MyPrivateMethod(); // Not OK, this will generate a compile error
-        MyProtectedMethod(); // OK, protected methods can be accessed
+        // PrivateMethod(); // Not OK, compile error
+        ProtectedMethod(); // OK, protected methods can be accessed
     }
 }
